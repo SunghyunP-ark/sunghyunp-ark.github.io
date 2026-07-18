031b35288c914d900ad40f8af73c2b03ebd7bdf3
Sunghyun Park <psh040@naver.com>
2026-07-18 17:22:19 +0900

    change

--- src/data/awards.js
+++ src/data/awards.js
@@ -2,13 +2,6 @@ export const awards = [
   {
     id: 1,
     title: 'Honors Award, Yonsei',
-    subtitle: 'Spring 2025',
-    description: 'Awarded to the top 10% students in Yonsei Students',
-    logo: '/static/img/yonsei_logo.png',
-  },
-  {
-    id: 2,
-    title: 'Honors Award, Yonsei',
     subtitle: 'Spring 2021',
     description: 'Awarded to the top 10% students in Yonsei Students',
     logo: '/static/img/yonsei_logo.png',
